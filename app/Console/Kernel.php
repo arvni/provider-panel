d16dda5ad9b4d18024ec9d4f861e7a5335586bef
Arvin Izadi <arvinizadi1991@gmail.com>
2024-02-17 03:06:07 +0400

    bug: test sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\SyncOrdersStatus;
 use App\Console\Commands\SyncReferrers;
+use App\Console\Commands\SyncTests;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
